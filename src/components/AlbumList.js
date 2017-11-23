981bbe00c9b2b3f2ffffc51f7d7139a658787859
Chris Rotis <zeuri@chriss-mbp.home>
2017-11-23 14:03:31 +0000

    First Commit r Please enter the commit message for your changes. Lines starting

---
+++ src/components/AlbumList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }     
+
+  renderAlbums() {
+    // return this.state.albums.map(album =>
+      // <AlbumDetail key={album.title} album={album} />
+      return this.state.albums.map(album => <Text>{album.title}</Text>);
+    // );
+  }
+
+  render () {
+    console.log(this.state);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
